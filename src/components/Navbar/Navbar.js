ad1369f57744d2abb74594a46efc47c7db1981c8
Samuel Lee <lee.sam1492@gmail.com>
2022-02-08 16:24:46 -0700

    added nav bar title for each page

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,27 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { useLocation } from "react-router-dom";
-
+// import { useLocation } from "react-router-dom";
 import { handleClick } from "../../Redux/sidenav";
 import { FaBars } from "react-icons/fa";
 import { CgProfile } from "react-icons/cg";
 import { ImCross } from "react-icons/im";
 import Login from "../Login/Login";
 import "./Navbar.css";
+import { useLocation } from "react-router-dom";
 
 const Navbar = () => {
-  const [pageTitle, setPageTitle] = useState("Shwifty Hoops");
-
-  const usePathname = () => {
-    const location = useLocation();
-    const pageTitle = location.pathname.split("/")[1].toUpperCase();
-    console.log(location.pathname);
-    return setPageTitle(pageTitle);
-  };
-
   const [showLogin, setShowLogin] = useState(false);
   const { sidenav } = useSelector((state) => state.sidenav);
   const dispatch = useDispatch();
+
+  const location = useLocation();
+  let pageTitle = location.pathname.split("/")[1];
+
   return (
     <div className="nav-wrapper">
       <div className="nav-bar">
@@ -36,7 +31,7 @@ const Navbar = () => {
             onClick={() => dispatch(handleClick(!sidenav))}
           />
         )}
-        <h1 className="nav-title">{}</h1>
+        <h1 className="nav-title">{pageTitle ? pageTitle : "Shwifty Hoops"}</h1>
         {/* <CgProfile
           className="three-dots"
           onClick={() => alert("working")}
